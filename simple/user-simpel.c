165397ae9e960b6e8fe47356363c471417efd674
briediz <dainius@dainius.de>
2017-11-02 17:23:35 +0100

    update

--- simple/user-simpel.c
+++ simple/user-simpel.c
@@ -13,40 +13,69 @@
 #include <sys/mman.h>
 
 
-#define FILESIZE 4096
+#define FILESIZERAM   0x10000000
+#define FILESIZECONF  0x00020000
+#define FILESIZEFLASH 0x02000000 
+
 int main(int argc, char* argv[]) {
  
     int input_fd;
     int *map, *ptr;
-    int i, fd;
+    int  fd, map_length, tmp;
+    unsigned long long i, offset, length;
     
-    /* Are src and dest file name arguments missing */
-    if(argc != 2){
-        printf ("Usage: file \n");
+    if(argc != 4){
+        printf ("Usage: read-programm device offset length\n");
         return 1;
     }
  
-    /* Create input file descriptor */
+    if(strcmp(argv [1], "/dev/ram")==0)
+        map_length=FILESIZERAM;
+    else if(strcmp(argv [1], "/dev/conf")==0)
+        map_length=FILESIZECONF;
+    else if(strcmp(argv [1], "/dev/flash")==0)
+        map_length=FILESIZEFLASH;
+    else {
+        perror("Error set file size");
+        exit(EXIT_FAILURE);
+        return -1;
+    }
+
     input_fd = open (argv [1], O_RDONLY);
     if (input_fd == -1) {
         perror ("open");
         return 2;
     }
     
-    map = mmap(0, FILESIZE, PROT_READ , MAP_SHARED, input_fd, 0);
+    map = mmap(0, map_length, PROT_READ , MAP_SHARED, input_fd, 0);
     ptr=map;
     if (map <= 0) {
         close(input_fd);
         perror("Error mmapping the file");
         exit(EXIT_FAILURE);
     } else {
-        printf ("mapping ok\n");
+        printf ("mapping ok, file open %s, size 0x%x\n", argv[1], map_length);
     }
-
-    printf("val=0x%08d\n", map[0] );
-    printf("val=0x%08d\n", map[1] );
-   
-    if (munmap(map, FILESIZE) == -1) {
+    
+    if (argv [2] != NULL)
+        offset =  atoi(argv[2]);
+    else
+        offset = 0;
+    
+    
+    if (argv [3] != NULL)
+        length = atoi(argv[3]);
+    else
+        length = 10;    
+    
+     
+    for( i=offset; i<length; i++){
+        printf("val[%08x]=0x%08x\n",i, map[i] );
+    }
+    
+    printf("read done %x bytes\n", map_length );
+    
+    if (munmap(map, map_length) == -1) {
         perror("Error un-mmapping the file");
     }
     
